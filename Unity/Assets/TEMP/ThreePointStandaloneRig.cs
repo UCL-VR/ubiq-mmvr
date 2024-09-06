f92054923013a62d669fd7f932710dd9683e45df
bnco-dev <33021110+bnco-dev@users.noreply.github.com>
2024-09-06 11:30:37 +0100

    Merge mm avatar into avatar system with networking

--- Unity/Assets/TEMP/ThreePointStandaloneRig.cs
+++ Unity/Assets/TEMP/ThreePointStandaloneRig.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,24 +11,54 @@ public class ThreePointStandaloneRig : MonoBehaviour
     public Transform left;
     public Transform right;
     public Transform head;
+    
+    [SerializeField]private Transform leftFallback;
+    [SerializeField]private Transform rightFallback;
 
-    private Ubiq.Avatars.AvatarManager avatarManager;
+    private HeadAndHandsAvatar headAndHandsAvatar;
 
-    void Start()
+    private void Start()
     {
-        avatarManager = FindAnyObjectByType<AvatarManager>();
+        headAndHandsAvatar = GetComponent<HeadAndHandsAvatar>();
+        headAndHandsAvatar.OnHeadUpdate.AddListener(HeadAndHandsAvatar_OnHead);
+        headAndHandsAvatar.OnLeftHandUpdate.AddListener(HeadAndHandsAvatar_OnLeftHand);
+        headAndHandsAvatar.OnRightHandUpdate.AddListener(HeadAndHandsAvatar_OnRightHand);
     }
 
-    void Update()
+    private void OnDestroy()
     {
-        if(avatarManager.input.TryGet(out IHeadAndHandsInput src))
+        if (!headAndHandsAvatar)
         {
-            left.position = src.leftHand.value.position;
-            left.rotation = src.leftHand.value.rotation;
-            right.position = src.rightHand.value.position;
-            right.rotation = src.rightHand.value.rotation;
-            head.position = src.head.value.position;
-            head.rotation = src.head.value.rotation;
+            return;
         }
+        
+        headAndHandsAvatar.OnHeadUpdate.RemoveListener(HeadAndHandsAvatar_OnHead);
+        headAndHandsAvatar.OnLeftHandUpdate.RemoveListener(HeadAndHandsAvatar_OnLeftHand);
+        headAndHandsAvatar.OnRightHandUpdate.RemoveListener(HeadAndHandsAvatar_OnRightHand);
+        headAndHandsAvatar = null;
+    }
+
+    private void HeadAndHandsAvatar_OnHead(InputVar<Pose> headPose)
+    {
+        if (!headPose.valid)
+        {
+            return;
+        }
+        
+        head.SetPositionAndRotation(headPose.value.position,headPose.value.rotation);
+    }
+    
+    private void HeadAndHandsAvatar_OnLeftHand(InputVar<Pose> leftHandPose)
+    {
+        left.SetPositionAndRotation(
+            leftHandPose.valid ? leftHandPose.value.position : leftFallback.position,
+            leftHandPose.valid ? leftHandPose.value.rotation : leftFallback.rotation);
+    }
+    
+    private void HeadAndHandsAvatar_OnRightHand(InputVar<Pose> rightHandPose)
+    {
+        right.SetPositionAndRotation(
+            rightHandPose.valid ? rightHandPose.value.position : rightFallback.position, 
+            rightHandPose.valid ? rightHandPose.value.rotation : rightFallback.rotation);
     }
 }
